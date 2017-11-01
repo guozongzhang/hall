61deda437c169040b2cace99a87b2f77bd9f71dd
yuguo6211 <yuguocug@sina.com>
2017-10-31 23:25:10 -0700

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,14 +3,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxt-switch-template-demo',
+    title: '搭配家云展厅',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/dpjia.ico' }
     ]
   },
   /*

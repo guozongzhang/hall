01ec40132b0de8923e2f08bf576d14e38ccefea9
yuguo6211 <yuguocug@sina.com>
2017-11-09 17:11:37 -0800

    注册

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,8 +2,7 @@ import axios from 'axios'
 export default axios.create({
   baseURL: process.env.baseUrl,
   headers: {
-    'X-DP-Key': 222,
-    'X-DP-ID': 111,
-    'X-DP-Token': '4be59d9215e5f48c772c560bcfb02fc0'
+    'X-DP-Key': '0c31e550cfdab86f2c2ea59327907798',
+    'X-DP-ID': 'cfdab86f2c2ea593'
   }
 })

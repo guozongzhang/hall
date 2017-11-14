71d4be7df9f92e13a316db22348ff544935a50e2
gyang <gy@dpjia.com>
2017-11-14 00:25:30 -0800

    tmp

---
+++ components/templates/default/furniture/detail.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="furniture">
+    <div class="fur_info">
+      <p class="title">银丰科技红烧云系列yyoowww</p>
+      <span class="bqian">新品上市特价三天</span>
+      <div class="price">
+        <span>优惠价<i>{{}}</i></span>
+        <span>原价<i>{{}}</i></span>
+        <span>专属价<i>{{}}</i></span>
+      </div>
+      <div class="scang">
+        收藏
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      data: {
+      }  
+    }
+  },
+  methods: {
+    init: function () {
+    }
+  },
+  mounted () {
+    this.init()
+  }
+}
+</script>
+
+<style scoped>
+.fur_info {
+  height: 140px;
+  margin-bottom: 10px;
+}
+.title {
+  height: 45px;
+  line-height: 50px;
+  font-size: 16px;
+}
+</style>
--- components/templates/default/person/basic.vue
+++ components/templates/default/person/basic.vue
@@ -2,8 +2,12 @@
   <div>
     <div class="personbg">
       <img class="bgimg" src="/images/person_bg.png">
-      <img class="bgheader" src="/images/shuijiao.jpg">
-      <p>点击登录</p>
+      <img :src="data.info3.header_img" class="bgheader" > 
+      <div class="name">
+        <p>
+          <span>{{data.info3.header_name}}<i v-if="data.info3.type != 'wait'" class="editheader"> </i></span>
+        </p>
+      </div>
     </div>
     <ul class="mui-table-view">
       <li class="personli">
@@ -12,7 +16,8 @@
           <div class="mui-media-body">
             <p class='mui-ellipsis'>
               手机号:
-              <span>-</span>
+              <span>{{data.info3.tel.num}}</span>
+              <span class="colorlan">{{data.info3.tel.txt}}</span>
             </p>
           </div>
         </a>
@@ -23,7 +28,8 @@
           <div class="mui-media-body">
             <p class='mui-ellipsis'>
               微信号:
-              <span>-</span>
+              <span>{{data.info3.wechet.first}}</span>
+              <span class="colorlan">{{data.info3.wechet.second}}</span>
             </p>
           </div>
         </a>
@@ -32,10 +38,11 @@
         <a href="javascript:;">
           <span class="head"></span>
           <div class="mui-media-body">
-            <p class='mui-ellipsis'>
+            <p :class="data.info3.type != 'wait' ? 'h16' : ''" class='mui-ellipsis' >
               当前身份:
-              <span>游客</span>
+              <span>{{data.info3.identity.txt}}</span>
             </p>
+            <i class="sjdesign" v-if="!data.info3.identity.none">升级为销售设计师,享受更多特权</i>
           </div>
         </a>
       </li>
@@ -45,7 +52,7 @@
           <div class="mui-media-body">
             <p class='mui-ellipsis'>
               我的收藏:
-              <a>{{data.fur_num}}</a>
+              <a>{{data.info3.fur_num}}</a>
               个商品
             </p>
           </div>
@@ -55,14 +62,14 @@
         <a href="javascript:;">
           <span class="set"></span>
           <div class="mui-media-body">
-            <p class='mui-ellipsis set'>
+            <p :class="data.info3.type == 'wait' ? 'colorhui' : ''" class="mui-ellipsis">
               设置
             </p>
           </div>
         </a>
       </li>
       <li class="personli">
-        <a href="javascript:;" class="mui-navigate-right">
+        <a href="" class="mui-navigate-right">
           <span class="about"></span>
           <div class="mui-media-body">
             <p class='mui-ellipsis'>
@@ -79,7 +86,88 @@ export default {
   data () {
     return {
       data: {
-        fur_num: 10
+        info: {
+          header_img: '/images/shuijiao.jpg',
+          type: 'wait',
+          fur_num: 10,
+          tel: {
+            num: '-',
+            txt: ''
+          },
+          wechet: {
+            first: '-',
+            second: ''
+          },
+          identity: {
+            txt: '游客',
+            none: true
+          },
+          about: '/about/ms'
+        },
+        // 账号登录后
+        info1: {
+          header_img: '/images/muwu.jpg',
+          header_name: '黄老板',
+          type: 'zhlogin',
+          fur_num: 10,
+          tel: {
+            num: 1393095412,
+            txt: ''
+          },
+          wechet: {
+            first: '暂无,马上去',
+            second: '绑定'
+          },
+          identity: {
+            txt: '嘉利信得 销售设计师',
+            none: true
+          },
+          about: '/about/ms'
+        },
+        // 微信登录后2
+        info2: {
+          header_img: '/images/muwu.jpg',
+          header_name: '黄老板',
+          type: 'wechatlogin',
+          fur_num: 10,
+          tel: {
+            num: 1393095412,
+            txt: '更换手机号',
+            href: ''
+          },
+          wechet: {
+            first: '已绑定',
+            second: '黄鑫',
+            href: ''
+          },
+          identity: {
+            txt: '嘉利信得普通用户',
+            none: false
+          },
+          about: '/about/ms'
+        },
+        // 微信登录后1
+        info3: {
+          header_img: '/images/muwu.jpg',
+          header_name: '黄老板',
+          type: 'wechatlogin1',
+          fur_num: 10,
+          tel: {
+            first: '暂无,马上去',
+            second: '绑定',
+            href: ''
+          },
+          wechet: {
+            first: '已绑定',
+            second: '黄鑫',
+            href: ''
+          },
+          identity: {
+            txt: '嘉利信得普通用户',
+            none: false
+          },
+          about: '/about/ms'
+        }
       }
     }
   },
@@ -94,6 +182,19 @@ export default {
 </script>
 
 <style scoped>
+  
+  .sjdesign {
+    position: absolute;
+    top: 23px;
+    font-size: 12px;
+    font-style: normal;
+  }
+  .colorlan{
+    color: blue
+  }
+  .name span{
+    display: inline-block
+  }
   img{
     display: inherit;
   }
@@ -119,13 +220,13 @@ export default {
     left: 50%;
     top: 50%;
     margin-left: -52px;
-    margin-top: -52px;
+    margin-top: -64px;
     border: 2px solid #fff;
     box-shadow: 0px 1px 5px #666;
   }
   .personbg p {
     margin: 0px;
-    margin-top: 48px;
+    margin-top: 42px;
     text-align: center;
     color: #5175CE;
     font-size: 16px;
@@ -177,10 +278,28 @@ export default {
   }
   p.mui-ellipsis span, p.mui-ellipsis a{
     margin-left: 10px;
+    width: auto;
+    margin-right: 0px;
+    display: inline
   }
 
   .personli p.set{
     color: #A3A3A3
+  }
+  .editheader{
+    background: url("/images/person.png") no-repeat;
+    display: inline-block;
+    height: 20px;
+    width: 25px;
+    background-size: 490px;
+    background-position: -448px -29px;
+    float: right;
+  }
+  .personli p.h16{
+    line-height: 16px;
+  }
+  .personli .colorhui {
+    color: #9C9C9C
   }
 
 </style>

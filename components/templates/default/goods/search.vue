827b515c402494c4f8a3aa3ac7018149a2ab9dfe
yuguo6211 <yuguocug@sina.com>
2017-11-15 00:35:57 -0800

    search

---
+++ components/templates/default/goods/search.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <div class="mui-input-row">
+    <input type="text" class="mui-input">
+  </div>
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    init: function () {
+    }
+  },
+  mounted () {
+    this.init()
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ pages/search.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+  <header class="mui-bar mui-bar-nav">
+    <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+    <h1 class="mui-title">搜索</h1>
+  </header>
+  <div class="mui-content">
+    <component :is="search"></component>
+  </div>
+</div>
+</template>
+<script>
+  export default {
+    head: {
+      title: '注册'
+    },
+    beforeCreate () {
+      // 注册组件 component(名字, 相对路径)
+      this.component('search', 'goods/search.vue')
+    },
+    methods: {
+      init: function () {}
+    },
+    mounted () {
+      this.init()
+    }
+  }
+</script>
+<style>
+  html,body{
+    background-color: #fff !important;
+  }
+  .mui-bar{
+    box-shadow: none;
+  }
+</style>

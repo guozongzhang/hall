a3298654cade87dc029e7fac0d9ec58f73150337
yuguo6211 <yuguocug@sina.com>
2017-12-12 18:29:55 -0800

    状态颜色、加载bug

---
+++ components/templates/default/report/detail.vue
@@ -0,0 +1,28 @@
+
+<template>
+<div>
+  <header class="mui-bar mui-bar-nav">
+    <a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
+    <h1 class="mui-title">项目详情</h1>
+    <a href="javascript:;" class="mui-pull-right register-link">...</a>
+  </header>
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+    init: function () {
+    }
+  },
+  mounted () {
+    this.init()
+  }
+}
+</script>
+
+<style>
+</style>

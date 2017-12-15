cc5a0384aa3967622bede1e595015a92957bce67
yuguo6211 <yuguocug@sina.com>
2017-12-15 03:55:26 -0800

    tt

--- components/templates/default/report/detail.vue
+++ components/templates/default/report/detail.vue
@@ -529,6 +529,7 @@ export default {
   },
   methods: {
     init: async function () {
+      model.activeTab = 'home'
       let myURL = url.parse(window.location.href)
       model.linkPath = '/' + myURL.pathname.split('/')[1]
       let urlObj = querystring.parse(myURL.query)
@@ -900,9 +901,6 @@ export default {
       }).then(function (data) {
         model.init()
         window.mui.toast('编辑项目信息成功')
-        setTimeout(function () {
-          model.activeTab = 'home'
-        }, 1000)
       }).catch(function (error) {
         if (error.response.data.message === 'token is invalid') {
           window.mui.toast('登录信息过期!')

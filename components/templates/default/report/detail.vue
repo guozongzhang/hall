2e40d751b1677b5d03825946c4b8c93ee6528453
yuguo6211 <yuguocug@sina.com>
2017-12-17 00:12:11 -0800

    编辑宝贝人

--- components/templates/default/report/detail.vue
+++ components/templates/default/report/detail.vue
@@ -399,41 +399,7 @@
       </div>
     </div>
     <div v-show="activeTab == 'editreport'" class="subbox-show">
-      <header class="mui-bar mui-bar-nav">
-        <a class="mui-icon mui-icon-left-nav mui-pull-left go-back" @click="goBack()">返回</a>
-        <span class="fa close-icon" @click="goHome()">×</span>
-        <h1 class="mui-title othertitle">编辑报备人信息</h1>
-        <a class="mui-icon mui-pull-right save-btn" @click="confEditReport()">提交</a>
-      </header>
-      <div class="textarea-box">
-        <div class="line-box"></div>
-        <div>
-          <div class="mui-input-row sub-input-box">
-						<label>姓名</label>
-						<input type="text" placeholder="输入姓名" v-model="reporter.name">
-					</div>
-          <div class="mui-input-row sub-input-box">
-						<label>项目关系</label>
-						<input type="text" placeholder="输入项目关系务" v-model="reporter.relationship">
-					</div>
-          <div class="mui-input-row sub-input-box">
-						<label>期望提成</label>
-						<input type="number" placeholder="输入期望提成" v-model="reporter.commission">
-					</div>
-          <div class="mui-input-row sub-input-box">
-						<label>项目优势</label>
-						<input type="text" placeholder="输入项目优势" v-model="reporter.ascendancy">
-					</div>
-          <div class="mui-input-row sub-input-box">
-						<label>联系电话</label>
-						<input type="text" placeholder="输入联系电话" v-model="reporter.tel">
-					</div>
-          <div class="mui-input-row sub-input-box">
-						<label>联系邮箱</label>
-						<input type="text" placeholder="输入联系邮箱" v-model="reporter.email">
-					</div>
-        </div>
-      </div>
+      <vue-editreport :report="reporter" @getEditReport="confEditReport"></vue-editreport>
     </div>
     <div v-show="activeTab == 'editcompetitors'" class="subbox-show">
       <header class="mui-bar mui-bar-nav">
@@ -496,6 +462,7 @@
 import axios from '~/plugins/axios'
 import Area from '../common/threelayer.vue'
 import proType from '../common/onelayer.vue'
+import editReport from './_editreport.vue'
 let dateJson = require('~/static/js/date.json')
 let url = require('url')
 let querystring = require('querystring')
@@ -572,7 +539,8 @@ export default {
   },
   components: {
     'vue-area': Area,
-    'vue-one': proType
+    'vue-one': proType,
+    'vue-editreport': editReport
   },
   methods: {
     init: async function () {
@@ -1192,68 +1160,38 @@ export default {
     // 编辑报备人信息
     editReport: function (id) {
       model.reporter = {
+        id: id,
         name: model.reportman.name,
         relationship: model.reportman.project_relation,
         commission: model.reportman.royalties_expectation,
         ascendancy: model.reportman.strengths,
         tel: model.reportman.tel,
-        email: model.reportman.email
+        email: model.reportman.email,
+        goback: false
       }
       model.activeTab = 'editreport'
       proId = id
     },
 
     // 确定提交报备人信息
-    confEditReport: function () {
-      let telval = /^1[3|4|5|7|8][0-9]{9}$/
-      if (!_.isEmpty(model.reporter.tel)) {
-        if (!telval.test(model.reporter.tel)) {
-          window.mui.toast('手机号格式错误!')
-          return false
-        }
+    confEditReport: function (obj) {
+      if (obj.goback) {
+        model.goBack()
+        return
       }
-      let emailval = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/
-      if (!_.isEmpty(model.reporter.email)) {
-        if (!emailval.test(model.reporter.email)) {
-          window.mui.toast('邮箱格式错误!')
-          return false
-        }
-      }
-      let param = {
+      model.reportman = {
         id: proId,
-        name: model.reporter.name || '',
-        project_relation: model.reporter.relationship || '',
-        royalties_expectation: model.reporter.commission || '',
-        strengths: model.reporter.ascendancy || '',
-        tel: model.reporter.tel,
-        email: model.reporter.email,
-        is_self: 'yes'
+        name: obj.name,
+        project_relation: obj.relationship,
+        royalties_expectation: obj.commission,
+        strengths: obj.ascendancy,
+        tel: obj.tel,
+        email: obj.email
       }
-      axios.put('functions/report/project_reportman', null, {
-        data: param
-      }).then(function (data) {
-        model.reportman = {
-          id: proId,
-          name: model.reporter.name,
-          project_relation: model.reporter.relationship,
-          royalties_expectation: model.reporter.commission,
-          strengths: model.reporter.ascendancy,
-          tel: model.reporter.tel,
-          email: model.reporter.email
-        }
-        window.mui.toast('编辑报备人信息成功')
-        setTimeout(function () {
-          model.activeTab = 'home'
-        }, 1000)
-      }).catch(function (error) {
-        if (error.response.data.message === 'token is invalid') {
-          window.mui.toast('登录信息过期!')
-          setTimeout(function () {
-            Cookies.set('dpjia-hall-token', '')
-            window.location.reload()
-          }, 2000)
-        }
-      })
+      window.mui.toast('编辑报备人信息成功')
+      setTimeout(function () {
+        model.activeTab = 'home'
+      }, 1000)
     },
 
     // 编辑竞争信息

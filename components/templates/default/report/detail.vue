4a1100a5e74ac3e750c35a0432a69279585ca115
yuguo6211 <yuguocug@sina.com>
2017-12-19 01:53:28 -0800

    test

--- components/templates/default/report/detail.vue
+++ components/templates/default/report/detail.vue
@@ -1053,6 +1053,7 @@ export default {
         risk_analysis: model.editpro.risk_analysis,
         project_attachment: JSON.stringify(model.editproImg)
       }
+      window.alert(new Date(model.editpro.invitation_time || 0))
       window.alert(param.invitation_time)
       axios.put('functions/report/project', null, {
         data: param

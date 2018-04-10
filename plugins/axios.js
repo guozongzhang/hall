4f640ac31fc6b157139512459acf14cf85c74477
yuguo6211 <yuguocug@sina.com>
2018-04-09 19:41:01 -0700

    测试数据

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 let Cookies = require('js-cookie')
 function getToken () {
-  console.log('端口号：', process.env.port)
   let token = Cookies.get('dpjia-hall-token-' + process.env.port)
-  console.log('token:', process.env.port)
   let obj
   if (token) {
     obj = {

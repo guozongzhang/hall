322339c4216d77de5531ffd708b9a018ea8e7902
yuguo6211 <yuguocug@sina.com>
2018-04-09 18:57:16 -0700

    log

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 let Cookies = require('js-cookie')
 function getToken () {
-  window.mui.toast('端口号：', process.env.port)
+  console.log('端口号：', process.env.port)
   let token = Cookies.get('dpjia-hall-token-' + process.env.port)
-  window.mui.toast('token:', process.env.port)
+  console.log('token:', process.env.port)
   let obj
   if (token) {
     obj = {

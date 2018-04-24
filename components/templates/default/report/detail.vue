1a102a3aa2b87ec91758b795d58f97ed9c2c284f
gyang <gy@dpjia.com>
2018-04-24 01:28:24 -0700

    修改报备记录过期判断

--- components/templates/default/report/detail.vue
+++ components/templates/default/report/detail.vue
@@ -213,7 +213,7 @@
                   <ul>
                     <li v-for="(sub, num) in reportLoglist">
                       <div class="li-box" v-bind:class="num == 0 ? 'first' : ''">
-                        <p v-if="(basicinfo.state == 'had_handle' || basicinfo.state == 'adopt') && sub.flow_remark">
+                        <p v-if="(basicinfo.state == 'had_handle' || basicinfo.state == 'adopt' || basicinfo.state == 'overdue') && sub.flow_remark">
                           <span class="last-white-line" v-show="num == (reportLoglist.length - 1)"></span>
                           <span class="pointer"></span>
                           <span>{{sub.flow_remark}}</span>
